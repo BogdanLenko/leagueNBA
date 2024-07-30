6656ff15664aad8cc59dea8827121563369ec139
Bogdan Lenko <bogdanlenko20@gmail.com>
2024-07-30 21:23:00 +0300

    30.07 sign in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {SubmitHandler, useForm} from "react-hook-form";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    interface IForm {
+        'email': string,
+        'password': string,
+    }
+
+    const {register, handleSubmit, formState: { errors }, formState} = useForm<IForm>({
+        mode: "onChange"
+    })
+
+    const onSubmit: SubmitHandler<IForm> = (data) => {
+        console.log(data)
+    }
+
+    const emailError = formState.errors['email']?.message
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder='enter your e-mail' {...register('email', {
+                required: 'this field is required.',
+                pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: 'Please enter a valid email address',
+                }
+            })} />
+            {emailError && <p style={{
+                color: 'red',
+            }}>{emailError}</p>}
+            <input type="password" placeholder='password' {...register('password', {
+                required: 'this field is required.',
+                minLength: 8,
+            })}/>
+            {errors.password && <p style={{color: 'red', fontSize: 16}}>{errors.password.type === 'minLength' ? 'Пароль должен быть не менее 8 символов' : "это поле обязательно"}</p>}
+            <button>submit</button>
+        </form>
     </>
   )
 }
+
 
 export default App
